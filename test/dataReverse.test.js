6a288c12a2e0e5d945cf88bd5bcc05aceb43a306
Heestolee <dlckdgml25@gmail.com>
2024-07-02 08:42:00 +0900

    feat: dataReverse complete

---
+++ test/dataReverse.test.js
@@ -0,0 +1,9 @@
+import { expect, describe, it } from "vitest";
+import dataReverse from "../lib/buildPileOfCubes";
+
+describe.skip("dataReverse", function () {
+  it("should be blah blah", function () {
+    expect(dataReverse([1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,0,1,0,1,0,1,0])).to.eql([1,0,1,0,1,0,1,0,0,0,0,0,1,1,1,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1]);
+    expect(dataReverse([0,0,1,1,0,1,1,0,0,0,1,0,1,0,0,1])).to.eql([0,0,1,0,1,0,0,1,0,0,1,1,0,1,1,0]);
+  })
+});
