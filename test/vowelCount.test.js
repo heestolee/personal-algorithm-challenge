eee1a2d3a28aabb0d00a44b33fc0455112a25645
Heestolee <dlckdgml25@gmail.com>
2024-08-01 23:15:28 +0900

    feat: vowelCount complete

---
+++ test/vowelCount.test.js
@@ -0,0 +1,12 @@
+import { expect, describe, it } from "vitest";
+import getCount from "../lib/vowelCount";
+
+describe("vowelCount", function () {
+  it("should be blah blah", function () {
+    expect(getCount("abracadabra")).to.eql(5);
+    expect(getCount("pear tree")).to.eql(4);
+    expect(getCount("o a kak ushakov lil vo kashu kakao")).to.eql(13);
+    expect(getCount("my pyx")).to.eql(0);
+    expect(getCount("tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty")).to.eql(168);
+  })
+});
